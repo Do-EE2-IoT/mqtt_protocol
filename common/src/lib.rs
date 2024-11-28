88a171672c64f93f253a3a557da773fc164a9ebf
Do-EE2-IoT <do30032003@gmail.com>
2024-11-28 06:08:45 +0000

    update test

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -10,7 +10,6 @@ pub mod utils;
 #[cfg(test)] // Đảm bảo rằng chỉ khi test thì module này mới được biên dịch
 mod tests {
     use crate::client::Client;
-    use tokio::time::timeout;
 
     #[tokio::test]
     async fn test_connect_timeout() {
@@ -35,88 +34,4 @@ mod tests {
         );
     }
 
-    #[tokio::test]
-    async fn test_connect_success() {
-        let mut client = Client::new(
-            "test_client".to_string(),
-            "test.mosquitto.org".to_string(),
-            1883,
-            None,
-            None,
-            60,
-            1,
-        )
-        .await
-        .unwrap();
-
-        // Giả lập phản hồi hợp lệ từ broker
-        let result = client.connect(10).await;
-
-        assert!(result.is_ok());
-    }
-
-    #[tokio::test]
-    async fn test_connect_invalid_response() {
-        let mut client = Client::new(
-            "test_client".to_string(),
-            "test.mosquitto.org".to_string(),
-            1883,
-            None,
-            None,
-            60,
-            1,
-        )
-        .await
-        .unwrap();
-
-        // Giả lập phản hồi không hợp lệ từ broker
-        let result = client.connect(2).await;
-
-        assert!(result.is_err());
-        assert!(result.unwrap_err().contains("must be check again"));
-    }
-
-    #[tokio::test]
-    async fn test_connect_failed() {
-        let mut client = Client::new(
-            "test_client".to_string(),
-            "test.mosquitto.org".to_string(),
-            1883,
-            None,
-            None,
-            60,
-            1,
-        )
-        .await
-        .unwrap();
-
-        // Giả lập không thể kết nối đến broker
-        let result = client.connect(2).await;
-
-        assert!(result.is_err());
-        assert!(result.unwrap_err().contains("Failed to connect to broker"));
-    }
-
-    #[tokio::test]
-    async fn test_connect_read_error() {
-        let mut client = Client::new(
-            "test_client".to_string(),
-            "test.mosquitto.org".to_string(),
-            1883,
-            None,
-            None,
-            60,
-            1,
-        )
-        .await
-        .unwrap();
-
-        // Giả lập lỗi khi đọc từ stream
-        let result = client.connect(2).await;
-
-        assert!(result.is_err());
-        assert!(result
-            .unwrap_err()
-            .contains("Can't read anything from broker"));
-    }
 }
